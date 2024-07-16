c32b3dc026e8fa373a8ee74c96f8f1f3b3bf6a44
furkangerem <furkangerem@hotmail.com>
2024-07-16 13:38:54 +0300

    Initial commit

---
+++ src/test/java/com/furkangerem/todo_list_app/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkangerem.todo_list_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
